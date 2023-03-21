f0a64c6a46e0c8a89d6498b9e075d71f21177461
pyatr <simon.garanin@mail.ru>
2023-03-21 11:25:42 +0700

    Build info

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@
 
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
@@ -14,7 +16,6 @@ const htmlWebpackPlugin = new HtmlWebpackPlugin({template: "./public/index.html"
 //htmlWebpackPlugin.userOptions.filename = "index.php";
 
 const config = {
-
     entry: "./src/index.tsx",
     output: {
         path: path.resolve(__dirname, "dist"),
@@ -26,21 +27,15 @@ const config = {
     plugins: [
         htmlWebpackPlugin,
         new MiniCssExtractPlugin(),
-<<<<<<< Updated upstream
-
-=======
         new CopyPlugin(
             {
                 patterns: [
                     {from: "./public/assets", to: "assets"},
-                    //TODO: сделать копирование по типу ./public/*.php в dist, сейчас создает папку public
-                    {from: "./public/login.html"},
                     {from: "./public/manifest.json"},
                     {from: "./public/robots.txt"}
                 ]
             }
         )
->>>>>>> Stashed changes
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
