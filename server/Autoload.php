7d0e8cc55e71b58730fc66078a08ce3198bb8c28
pyatr <simon.garanin@mail.ru>
2023-04-13 19:44:42 +0700

    FUA-23 - backend - refactor autoloading

--- server/Autoload.php
+++ server/Autoload.php
@@ -2,14 +2,16 @@
 
 spl_autoload_register(function ($className) {
     $directories = [
-        "",
-        "Controllers\\",
-        "Models\\"
+        "Controllers",
+        "Models"
     ];
-    foreach ($directories as $dir) {
-        $file = __DIR__ . '\\' . $dir . $className . '.php';
+    $projectDir = __DIR__ . '\\';
+    $prefix = "Server\\";
+    $prefixLength = strlen($prefix);
+    $cleanClassName = substr($className, $prefixLength);
+    foreach ($directories as $classDir) {
+        $file = $projectDir . $classDir . "\\" . $cleanClassName . '.php';
         $file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
-
         if (file_exists($file)) {
             include $file;
         }
