e8cd093745c44e2442538c366078e036620dfa1e
pyatr <simon.garanin@mail.ru>
2023-04-13 19:44:19 +0700

    FUA-23 - backend - refactor base site

--- server/index.php
+++ server/index.php
@@ -1,16 +1,8 @@
 <?php
-
 include_once "Autoload.php";
 
 $decodedData = (array)json_decode(file_get_contents("php://input"));
 
-$api = new API();
+$api = new Server\API();
 
 $api->parseRequest($decodedData);
-//Test requests
-//$api->parseRequest(array("request" => "login", "params" => array("email" => "admin", "password" => "admin")));
-//echo $api->parseRequest(array("request" => "createPost", "params" => array("title" => "The afsdf", "shortTitle" => "Short title","content" => "Very cool thing", "categories" => "none")));
-//echo $api->parseRequest(array("request" => "deletePost", "params" => array("number" => "17")));
-//echo $api->parseRequest(array("request" => "updatePost", "params" => array("number" => "3", "title" => "Newer title", "shortTitle" => "Shorter title","content" => "Mediocre thing", "categories" => "scripts")));
-//echo $api->parseRequest(array("request" => "getPost", "params" => array("number" => "24")));
-//echo $api->parseRequest(array("request" => "getPosts", "params" => array("pageSize" => "6", "page"=>"2")));
