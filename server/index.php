a4ef2f7a1fdfb7063873c72c5941225b926d49f6
pyatr <simon.garanin@mail.ru>
2023-04-14 17:55:54 +0700

    FUA-23 - backend - update index.php

--- server/index.php
+++ server/index.php
@@ -1,8 +1,6 @@
 <?php
 include_once "Autoload.php";
 
-$decodedData = (array)json_decode(file_get_contents("php://input"));
-
 $api = new Server\API();
 
-$api->parseRequest($decodedData);
+$api->parseRequest();
