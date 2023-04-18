d51378f8811c0d0d572112a9baa36c9a460966d6
pyatr <simon.garanin@mail.ru>
2023-04-18 11:42:51 +0700

    FUA-6 - frontend - add item display text scaling

--- src/components/AssetItemDisplay.tsx
+++ src/components/AssetItemDisplay.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import {AssetItem} from "../pages/AssetsPage/AssetsPage";
 import {Grid, Typography} from "@mui/material";
 //If you get react-scale-text is not a module error, create react-scale-text.d.ts in node_modules package and add "declare module 'react-scale-text';"
-//Also crashes
-import {ScaleText} from "react-scale-text"
+//TODO: try use for scaling import {ScaleText} from "react-scale-text"
 
 export type AssetItemDisplay = {
     itemData: AssetItem,
@@ -14,17 +13,22 @@ export default function AssetItemDisplay({itemData, itemStyle}: AssetItemDisplay
     const gridStyle = {
         padding: "4px"
     }
-
+    const startPictureWidth = 187;
+    let fontSizeMod = parseFloat(itemStyle.width as string) / startPictureWidth;
     const imageStyle = {
         width: itemStyle.width,
         height: itemStyle.width
     }
+    const titleFontSize = fontSizeMod + "rem";
+    const subtitleFontSite = (fontSizeMod * 0.7) + "rem";
+    console.log([titleFontSize, subtitleFontSite]);
     return (<Grid item style={itemStyle} sx={{boxSizing: "content-box"}}>
         {<img src={itemData.TITLEPIC_LINK} style={imageStyle}/>}
         <Grid style={gridStyle}>
-            <Typography component="h6" fontSize="1rem">{itemData.SHORTTITLE}</Typography>
-            <Typography variant="subtitle1" color="#999999" fontSize="0.7rem">{itemData.CATEGORIES}</Typography>
-            <Typography variant="subtitle1" fontSize="0.7rem">{itemData.CREATION_DATE}</Typography>
+            <Typography component="h6" fontSize={titleFontSize}>{itemData.SHORTTITLE}</Typography>
+            <Typography variant="subtitle1" color="#999999"
+                        fontSize={subtitleFontSite}> {itemData.CATEGORIES}</Typography>
+            <Typography variant="subtitle1" fontSize={subtitleFontSite}>{itemData.CREATION_DATE}</Typography>
         </Grid>
     </Grid>);
 }
