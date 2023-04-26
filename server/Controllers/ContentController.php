f303cbee10bf939d86b86a42eced905a9be08301
pyatr <simon.garanin@mail.ru>
2023-04-26 17:42:38 +0700

    FUA 11 - backend - server/
    Add multiple image support for content creation
    Add support for different tables
    Remove short title

--- server/Controllers/ContentController.php
+++ server/Controllers/ContentController.php
@@ -6,29 +6,37 @@ class ContentController extends BaseController
 {
     private ContentModel $contentModel;
 
-    private string $titlepicFolder;
+    private string $serverRoot = "/var/www/html/server";
+
+    private array $tablesForCategories;
+    private array $foldersForCategories;
 
     public function __construct()
     {
         $this->contentModel = new ContentModel();
-        $this->titlepicFolder = ':' . $_SERVER['SERVER_PORT'] . '/TitlePics/';
+        $this->tablesForCategories = array("asset" => "ASSETS", "article" => "ARTICLES", "script" => "SCRIPTS");
+        $this->foldersForCategories = array("asset" => "Assets", "article" => "Articles", "script" => "Scripts");
     }
 
     public function createContent($params): array
     {
+        $category = $this->tryGetValue($params, 'category');
+        $tableName = $this->tablesForCategories[$category];
         $title = $this->tryGetValue($params, 'title');
-        $shortTitle = $this->tryGetValue($params, 'shortTitle');
         $content = $this->tryGetValue($params, 'content');
         $categories = $this->tryGetValue($params, 'categories');
-        $response = $this->contentModel->createContent($title, $shortTitle, $content, $categories);
+        $response = $this->contentModel->createContent($tableName, $title, $content, $categories);
         //Enable image saving (and probably more): /var/www/html/server# chmod -R 777 TitlePics
         $lastID = $this->contentModel->getLastPostID();
+        $folder = $this->foldersForCategories[$category];
         if ($response['result'] == 'success') {
-            $preview = $this->tryGetValue($params, 'preview');
-            $saveLocation = "/var/www/html/server/TitlePics/$lastID.png";
-            //echo $saveLocation . $preview;
-            $savedImage = file_get_contents($preview);
-            file_put_contents($saveLocation, $savedImage);
+            $galleryDirectory = "$this->serverRoot/Images/$folder/$lastID/Gallery";
+            mkdir($galleryDirectory);
+            $gallery = $this->tryGetValue($params, 'gallery');
+            foreach ($gallery as $image) {
+                $guid = GUIDCreator::GUIDv4();
+                file_put_contents("$galleryDirectory/$guid.png", file_get_contents($image));
+            }
         }
         $response['content']['itemID'] = $lastID;
         return $response;
@@ -36,36 +44,47 @@ class ContentController extends BaseController
 
     public function deleteContent($params): array
     {
+        $category = $this->tryGetValue($params, 'category');
+        $tableName = $this->tablesForCategories[$category];
         $contentNumber = $this->tryGetValue($params, 'number');
-        return $this->contentModel->deleteContent($contentNumber);
+        return $this->contentModel->deleteContent($tableName, $contentNumber);
     }
 
     public function updateContent($params): array
     {
+        $category = $this->tryGetValue($params, 'category');
+        $tableName = $this->tablesForCategories[$category];
         $contentNumber = $this->tryGetValue($params, 'number');
         $title = $this->tryGetValue($params, 'title');
-        $shortTitle = $this->tryGetValue($params, 'shortTitle');
         $content = $this->tryGetValue($params, 'content');
         $categories = $this->tryGetValue($params, 'categories');
-        return $this->contentModel->updateContent($contentNumber, $title, $shortTitle, $content, $categories);
+        return $this->contentModel->updateContent($tableName, $contentNumber, $title, $content, $categories);
     }
 
     public function getContent($params): array
     {
+        $category = $this->tryGetValue($params, 'category');
+        $tableName = $this->tablesForCategories[$category];
         $contentNumber = $this->tryGetValue($params, 'number');
-        return $this->contentModel->getContent($contentNumber);
+        return $this->contentModel->getContent($tableName, $contentNumber);
     }
 
     public function getContentPreviews($params): array
     {
+        $category = $this->tryGetValue($params, 'category');
+        $tableName = $this->tablesForCategories[$category];
         $pageSize = $this->tryGetValue($params, 'pageSize');
         $page = $this->tryGetValue($params, 'page');
-        $result = $this->contentModel->getContentPreviews($pageSize, $page);
+        $result = $this->contentModel->getContentPreviews($tableName, $pageSize, $page);
+
         $resultCount = count($result['content']);
+        $folder = $this->foldersForCategories[$category];
+
         for ($i = 0; $i < $resultCount; $i++) {
             $contentNumber = $result['content'][$i]['NUMBER'];
-            $name = $this->titlepicFolder . $contentNumber . '.png';
-            $result['content'][$i]['TITLEPIC_LINK'] = $name;
+            $directory = "/Images/$folder/$contentNumber/Gallery";
+            $previewName = scandir("$this->serverRoot/$directory/")[2];
+            $result['content'][$i]['titlepicLink'] = ":$_SERVER[SERVER_PORT]/$directory/$previewName";
         }
         return $result;
     }
--- server/Models/ContentModel.php
+++ server/Models/ContentModel.php
@@ -6,20 +6,38 @@ use PDO;
 
 class ContentModel extends BaseModel
 {
-    private const TABLE_POSTS = 'POSTS';
-
     private const ENTRY_NUMBER = 'NUMBER';
     private const ENTRY_TITLE = 'TITLE';
-    private const ENTRY_SHORTTITLE = 'SHORTTITLE';
     private const ENTRY_CONTENT = 'CONTENT';
     private const ENTRY_CATEGORIES = 'CATEGORIES';
     private const ENTRY_CREDATE = 'CREATION_DATE';
 
-    private const SHORT_DESC_LENGTH = 256;
+    private const SHORT_DESC_LENGTH = 128;
 
-    public function createContent(string $title, string $shortTitle, string $content, string $categories): array
+    public function createContent(string $tableName, string $title, string $content, string $categories): array
     {
-        $query = "INSERT INTO " . $this::TABLE_POSTS . " (" . $this::ENTRY_TITLE . "," . $this::ENTRY_SHORTTITLE . "," . $this::ENTRY_CONTENT . "," . $this::ENTRY_CATEGORIES . ") VALUES('$title', '$shortTitle', '$content', '$categories')";
+        $query = "INSERT INTO $tableName (" . $this::ENTRY_TITLE . "," . $this::ENTRY_CONTENT . "," . $this::ENTRY_CATEGORIES . ") VALUES('$title', '$content', '$categories')";
+        return $this->executeRequest($query);
+    }
+
+    public function updateContent(string $tableName, int $contentNumber, string $title, string $content, string $categories): array
+    {
+        $query = "UPDATE $tableName SET "
+            . $this::ENTRY_TITLE . "='$title',"
+            . $this::ENTRY_CONTENT . "='$content',"
+            . $this::ENTRY_CATEGORIES . "='$categories' WHERE " . $this::ENTRY_NUMBER . " = '$contentNumber'";
+        return $this->executeRequest($query);
+    }
+
+    public function deleteContent(string $tableName, int $contentNumber): array
+    {
+        $query = "DELETE FROM $tableName WHERE " . $this::ENTRY_NUMBER . " = '$contentNumber'";
+        return $this->executeRequest($query);
+    }
+
+    public function getContent(string $tableName, int $contentNumber): array
+    {
+        $query = "SELECT * FROM $tableName WHERE NUMBER = $contentNumber";
         return $this->executeRequest($query);
     }
 
@@ -29,17 +47,30 @@ class ContentModel extends BaseModel
         return $this->executeRequest($query)['content'][0]['LAST_INSERT_ID()'];
     }
 
-    public function updateContent(int $contentNumber, string $title, string $shortTitle, string $content, string $categories): array
+    public function getContentPreviews(string $tableName, int $pageSize, int $page): array
     {
-        $query = "UPDATE " . $this::TABLE_POSTS . " SET "
-            . $this::ENTRY_TITLE . "='$title',"
-            . $this::ENTRY_SHORTTITLE . "='$shortTitle',"
-            . $this::ENTRY_CONTENT . "='$content',"
-            . $this::ENTRY_CATEGORIES . "='$categories' WHERE " . $this::ENTRY_NUMBER . " = '$contentNumber'";
-        return $this->executeRequest($query);
+        $postsOffset = $pageSize * ($page - 1);
+        $query = "SELECT " . $this::ENTRY_NUMBER . ","
+            . $this::ENTRY_TITLE . ","
+            . $this::ENTRY_CATEGORIES . ","
+            . $this::ENTRY_CONTENT . " FROM $tableName ORDER BY 1 DESC LIMIT $pageSize OFFSET $postsOffset";
+        $result = $this->executeRequest($query);
+        for ($i = 0; $i < count($result['content']); $i++) {
+            $result['content'][$i]['CONTENT'] = substr($result['content'][$i]['CONTENT'], 0, $this::SHORT_DESC_LENGTH);
+        }
+        $result['contentCount'] = $this->getContentCount($tableName);
+        return $result;
     }
 
-    public function executeRequest($query): array
+    public function getContentCount(string $tableName,)
+    {
+        $query = "SELECT COUNT(*) FROM $tableName;";
+        $req = $this->DBConn->prepare($query);
+        $req->execute();
+        return $req->fetch(PDO::FETCH_NAMED)['COUNT(*)'];
+    }
+
+    protected function executeRequest($query): array
     {
         $response = array('result' => 'success');
         $req = $this->DBConn->prepare($query);
@@ -52,40 +83,5 @@ class ContentModel extends BaseModel
         $response['content'] = $req->fetchAll(PDO::FETCH_NAMED);
         $response['code'] = $req->errorCode();
         return $response;
-    }
-
-    public function getContent(int $contentNumber): array
-    {
-        $query = 'SELECT * FROM ' . $this::TABLE_POSTS . ' WHERE NUMBER = ' . "$contentNumber";
-        return $this->executeRequest($query);
-    }
-
-    public function getContentPreviews(int $pageSize, int $page): array
-    {
-        $postsOffset = $pageSize * ($page - 1);
-        $query = "SELECT " . $this::ENTRY_NUMBER . ","
-            . $this::ENTRY_SHORTTITLE . ","
-            . $this::ENTRY_CATEGORIES . ","
-            . $this::ENTRY_CONTENT . " FROM " . $this::TABLE_POSTS . " ORDER BY 1 DESC LIMIT $pageSize OFFSET $postsOffset";
-        $result = $this->executeRequest($query);
-        for ($i = 0; $i < count($result['content']); $i++) {
-            $result['content'][$i]['CONTENT'] = substr($result['content'][$i]['CONTENT'], 0, $this::SHORT_DESC_LENGTH);
-        }
-        $result['contentCount'] = $this->getContentCount();
-        return $result;
-    }
-
-    public function getContentCount()
-    {
-        $query = 'SELECT COUNT(*) FROM ' . $this::TABLE_POSTS . ';';
-        $req = $this->DBConn->prepare($query);
-        $req->execute();
-        return $req->fetch(PDO::FETCH_NAMED)['COUNT(*)'];
-    }
-
-    public function deleteContent(int $contentNumber): array
-    {
-        $query = "DELETE FROM " . $this::TABLE_POSTS . " WHERE " . $this::ENTRY_NUMBER . " = '$contentNumber'";
-        return $this->executeRequest($query);
     }
 }
