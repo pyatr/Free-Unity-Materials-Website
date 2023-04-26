5c43feb2779b845d6b663b0a43c88aefd469e4db
pyatr <simon.garanin@mail.ru>
2023-04-26 19:17:02 +0700

    FUA 11 - frontend - utils/Login.ts refactor login

--- src/utils/Login.ts
+++ src/utils/Login.ts
@@ -6,53 +6,54 @@ import {GoToHomePage} from "./GoToHomePage";
 export async function TryCookieLogin() {
     const cookie = new Cookies();
     let loginCookie = cookie.get("userLogin");
-    if (loginCookie != null) {
-        let scon = new ServerConnection();
-        sessionStorage.removeItem("finishedWaiting");
-        await scon.SendPostRequest("loginCookie", {}, OnCookieLoginResponse);
-    } else {
+    if (loginCookie == null) {
         ClearUserData();
-        sessionStorage.setItem("finishedWaiting", "true");
+        return;
+    }
+    if (!IsLoggedIn()) {
+        let serverConnection = new ServerConnection();
+        sessionStorage.setItem("userLoginStatus", "loading");
+        const {data} = await serverConnection.SendPostRequestPromise("loginCookie", {});
+        const loginStatus = data.loginStatus.toString();
+        if (loginStatus !== "success") {
+            //Delete login cookie if it failed to log us in
+            ClearUserData();
+        } else {
+            sessionStorage.setItem("userLoginStatus", loginStatus);
+            sessionStorage.setItem("userName", data.userName);
+            sessionStorage.setItem("userRole", data.userRole);
+        }
     }
 }
 
 export async function TryLogin(email: string, password: string) {
-    let scon: ServerConnection;
-    scon = new ServerConnection();
+    let serverConnection: ServerConnection = new ServerConnection();
 
     const params = {
         email: email,
         password: password
     };
-    await scon.SendPostRequest("login", params, OnLoginResponse);
-}
-
-function OnCookieLoginResponse(response: AxiosResponse) {
-    var loginStatus = response.data.loginStatus.toString();
-    if (loginStatus !== "success") {
-        //Delete login cookie if it failed to log us in
-        ClearUserData();
-    } else {
-        sessionStorage.setItem("userLoginStatus", loginStatus);
-        sessionStorage.setItem("userName", response.data.userName);
-        sessionStorage.setItem("userRole", response.data.userRole);
-    }
-    sessionStorage.setItem("finishedWaiting", "true");
-}
-
-function OnLoginResponse(response: AxiosResponse) {
+    const {data} = await serverConnection.SendPostRequestPromise("login", params);
     try {
-        if (response.data.loginStatus !== "success") {
+        if (data.loginStatus !== "success") {
             return;
         }
         const cookies = new Cookies();
         const date = Date.now();
         const expirationDate = new Date(date + 1000 * 60 * 60 * 24);
-        cookies.set("userLogin", response.data.loginCookie, {expires: expirationDate});
+        cookies.set("userLogin", data.loginCookie, {expires: expirationDate});
         GoToHomePage();
     } catch (e) {
         console.log("Error when parsing login response: " + e);
     }
+}
+
+export function IsLoggedIn(): boolean {
+    return sessionStorage.getItem("userLoginStatus") === "success";
+}
+
+export function IsLoading(): boolean {
+    return sessionStorage.getItem("userLoginStatus") === "loading";
 }
 
 export function LogOut() {
@@ -74,5 +75,4 @@ export function ClearUserData() {
     sessionStorage.removeItem("userLoginStatus");
     sessionStorage.removeItem("userName");
     sessionStorage.removeItem("userRole");
-    sessionStorage.removeItem("finishedWaiting");
 }
