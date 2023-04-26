e07fda63c154978b8667b90c8b8afa52e00dc3bb
pyatr <simon.garanin@mail.ru>
2023-04-26 19:18:28 +0700

    FUA 11 - frontend - components/ separate user display from bar, remove login wait timer

--- src/components/SiteAppBar.tsx
+++ src/components/SiteAppBar.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment, useEffect, useState} from "react";
+import React from "react";
 
 import {Link} from "react-router-dom";
 import {
@@ -6,77 +6,9 @@ import {
     Box,
     Toolbar,
     Avatar,
-    CssBaseline,
-    Typography,
-    Grid
+    CssBaseline
 } from "@mui/material";
-import {IsMobileResolution} from "../utils/MobileUtilities";
-import {LogOut} from "../utils/Login";
-
-function WaitForLogin(onFinishedWaiting: Function) {
-    let finishedWaiting = (sessionStorage.getItem("finishedWaiting") === "true");
-
-    if (finishedWaiting) {
-        onFinishedWaiting(false);
-        return;
-    }
-    const loginCheckIntervalMs = 10;
-    const loginTimeMs = 8000;
-
-    const loginInterval = setInterval(() => {
-        finishedWaiting = (sessionStorage.getItem("finishedWaiting") === "true");
-        if (finishedWaiting) {
-            onFinishedWaiting(false);
-            clearInterval(loginInterval);
-        }
-    }, loginCheckIntervalMs);
-
-    const loginWaitTimer = setTimeout(() => {
-        onFinishedWaiting(false);
-        clearInterval(loginInterval);
-        clearTimeout(loginWaitTimer);
-        finishedWaiting = true;
-    }, loginTimeMs);
-}
-
-function AuthorizationResult() {
-    const [isLoading, setLoadingStatus] = useState(true);
-    let isMobile = IsMobileResolution();
-    let fontsize = isMobile ? 12 : 18;
-    let fontStyle = {fontSize: fontsize, color: "#000000", width: "fit-content"};
-
-    let isLoggedIn = (sessionStorage.getItem("userLoginStatus") === "success");
-
-    useEffect(() => {
-        WaitForLogin(setLoadingStatus);
-    }, []);
-
-    return (
-        <Grid
-            container
-            direction="column"
-            justifyContent="flex-end"
-            alignItems="left"
-            marginTop="0px"
-            marginLeft="0px"
-            spacing={2}
-            sx={{
-                width: "20%"
-            }}>
-            {isLoading ?
-                <Typography style={fontStyle}>Loading...</Typography> :
-                isLoggedIn ?
-                    <Fragment>
-                        <Typography style={fontStyle}>{sessionStorage.getItem("userName")}</Typography>
-                        <Link style={fontStyle} to="/assets" onClick={LogOut}>Log out</Link>
-                    </Fragment> :
-                    <Fragment>
-                        <Link style={fontStyle} to="/login">Log in</Link>
-                        <Link style={fontStyle} to="/register">Register</Link>
-                    </Fragment>
-            }
-        </Grid>);
-}
+import UserBarDisplay from "./UserBarDisplay";
 
 export default function SiteAppBar() {
     return (
@@ -95,7 +27,7 @@ export default function SiteAppBar() {
                             height: "100%",
                             margin: "0.5%"
                         }}/>
-                    <AuthorizationResult/>
+                    <UserBarDisplay/>
                 </Toolbar>
             </AppBar>
         </Box>
---
+++ src/components/UserBarDisplay.tsx
@@ -0,0 +1,40 @@
+import {IsMobileResolution} from "../utils/MobileUtilities";
+import {IsLoading, IsLoggedIn, LogOut} from "../utils/Login";
+import {Grid, Typography} from "@mui/material";
+import React, {Fragment} from "react";
+import {Link} from "react-router-dom";
+
+export default function UserBarDisplay() {
+    let isMobile = IsMobileResolution();
+    let fontsize = isMobile ? 12 : 18;
+    let fontStyle = {fontSize: fontsize, color: "#000000", width: "fit-content"};
+
+    const isLoggedIn = IsLoggedIn();
+    const isLoading = IsLoading();
+
+    return (
+        <Grid
+            container
+            direction="column"
+            justifyContent="flex-end"
+            alignItems="left"
+            marginTop="0px"
+            marginLeft="0px"
+            spacing={2}
+            sx={{
+                width: "20%"
+            }}>
+            {isLoading ?
+                <Typography style={fontStyle}>Loading...</Typography> :
+                isLoggedIn ?
+                    <Fragment>
+                        <Typography style={fontStyle}>{sessionStorage.getItem("userName")}</Typography>
+                        <Link style={fontStyle} to="/assets" onClick={LogOut}>Log out</Link>
+                    </Fragment> :
+                    <Fragment>
+                        <Link style={fontStyle} to="/login">Log in</Link>
+                        <Link style={fontStyle} to="/register">Register</Link>
+                    </Fragment>
+            }
+        </Grid>);
+}
